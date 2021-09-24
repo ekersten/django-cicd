dd4353c8d696dbbc80d20287d6920f85728173d3
Eric Kersten <ekersten@gmail.com>
2021-09-24 18:59:25 -0300

    fix test

--- website/views.py
+++ website/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-    return render(request, 'website/contacto.html')
+    return render(request, 'website/index.html')
 
 def contacto_view(request):
     return render(request, 'website/contacto.html')
